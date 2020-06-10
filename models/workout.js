03314d6e071f494dfd2553ac499c6a363114c53b
RyanJMS <59546790+RyanJMS@users.noreply.github.com>
2020-06-09 22:24:39 -0400

    added date / excercises to workout.js model

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,50 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({});
+const workoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: { type: String, trim: true, required: "Enter the exercise type" },
+    },
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter the exercise name",
+      },
+    },
+    {
+      duration: {
+        type: Number,
+        required: "Enter the exercise duration (mins)",
+      },
+    },
+    {
+      distance: {
+        type: Number,
+      },
+    },
+    {
+      reps: {
+        type: Number,
+      },
+    },
+    {
+      sets: {
+        type: Number,
+      },
+    },
+    {
+      weight: {
+        type: Number,
+      },
+    },
+  ],
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
